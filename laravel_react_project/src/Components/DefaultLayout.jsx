5d47736ae82bf58867f0f44daa7730ba730d8dbc
AbdouniAbdessamad <abdouni.abdou4@gmail.com>
2024-05-29 00:18:55 +0100

    Added inventaire

--- laravel_react_project/src/Components/DefaultLayout.jsx
+++ laravel_react_project/src/Components/DefaultLayout.jsx
@@ -1,74 +1,90 @@
-
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navigate, Outlet } from "react-router-dom";
 import axiosClient from "../axiosClient";
 import { useStateContext } from "../contexts/contextprovider";
 
-export default function DefaultLayout(){
-    const {user, token, setUser, setToken} = useStateContext();
-    if(!token){
-       return <Navigate to='/login'/>
+export default function DefaultLayout() {
+    const { user, token, setUser, setToken } = useStateContext();
+    if (!token) {
+        return <Navigate to="/login" />;
     }
 
-    const onLogout =  (ev) =>{
+    const onLogout = (ev) => {
         ev.preventDefault();
-        axiosClient.get('/logout')
-        .then(({}) => {
-           setUser(null)
-           setToken(null)
-        })
-    }
+        axiosClient.get("/logout").then(({}) => {
+            setUser(null);
+            setToken(null);
+        });
+    };
 
     useEffect(() => {
-        axiosClient.get('/user')
-          .then(({data}) => {
-             setUser(data)
-          })
-      }, [])
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
 
-      return (
+    return (
         <div id="defaultLayout">
-          <div className="content">
-            <header>
-            <div className="title">
-               ABDOUNISM
-              </div>
-              {
-  user && user.id === 1 // Check if user object exists and its ID is 1
-    ? // If the condition is true, render the following JSX
-      <div className="NavBarElements">
-        <Link className="navlink" to="/users">Utilisateurs</Link>
-      </div>
-    : // If the condition is false, render nothing
-      null
-}
+            <div className="content">
+                <header>
+                    <div className="title">ABDOUNISM</div>
+                    {user && user.id === 1 ? ( // Check if user object exists and its ID is 1
+                        // If the condition is true, render the following JSX
+                        <div className="NavBarElements">
+                            <Link className="navlink" to="/users">
+                                Utilisateurs
+                            </Link>
+                        </div>
+                    ) : // If the condition is false, render nothing
+                    null}
 
-            <div className="NavBarElements">
-              <Link className="navlink" to='/articles'>Articles</Link>
+                    <div className="NavBarElements">
+                        <Link className="navlink" to="/articles">
+                            Articles
+                        </Link>
+                    </div>
+                    <div className="NavBarElements">
+                        <Link className="navlink" to="/suppliers">
+                            Fournisseurs
+                        </Link>
+                    </div>
+                    <div className="NavBarElements">
+                        <Link className="navlink" to="/category">
+                            Categories
+                        </Link>
+                    </div>
+                    <div className="NavBarElements">
+                        <Link className="navlink" to="/QuantiteEx">
+                            Quantité existante
+                        </Link>
+                    </div>
+                    <div className="NavBarElements">
+                        <Link
+                            className="navlink"
+                            to="/Inventaire
+"
+                        >
+                            Inventaire
+                        </Link>
+                    </div>
+                    <div>
+                        <span className="px-2 mx-6">
+                            <strong>
+                                <Link to="/profile">{user.name}</Link>
+                            </strong>
+                        </span>
+                        &nbsp;&nbsp;
+                        <a href="#" onClick={onLogout} className="btn-logout">
+                            {" "}
+                            Logout
+                        </a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
             </div>
-              <div className="NavBarElements">
-               <Link className="navlink" to='/suppliers'>Fournisseurs</Link>
-              </div>
-              <div className="NavBarElements">
-                <Link className="navlink" to='/category'>Categories</Link>
-              </div>
-              <div className="NavBarElements">
-                <Link className="navlink" to='/QuantiteEx'>Quantité existante</Link>
-              </div>
-              <div>
-                <span className="px-2 mx-6">
-                  <strong><Link to='/profile'>{user.name}</Link></strong>
-                </span>
-                &nbsp;&nbsp;
-                <a href="#" onClick={onLogout} className="btn-logout"> Logout</a>
-            </div>
-            </header>
-            <main>
-              <Outlet />
-            </main>
-          </div>
         </div>
-      );
-
+    );
 }
